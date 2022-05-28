05e44b88966b3af8b00f5b89cbb822e39c3d30a7
Priyanka2503 <97089850+Priyankaj2503@users.noreply.github.com>
2022-05-28 14:43:41 +0000

    problem 8

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,18 +1,5 @@
-fname = input("Enter the file name: ")
-try:
-	fn = open(fname)
-except:
-        print("The file name doesn't exist")
-        quit()
-count = 0
-total = 0
-for line in fn:        	    
-    if line.startswith("X-DSPAM-Confidence:"):
-        count = count + 1
-        fline = line.find(':')
-        num = line[fline+1:].strip()    
-        S = float(num)
-        total = total + S
-average = total / count
-print('Average spam confidence:', average)
- #code is reviewed
+text = "X-DSPAM-Confidence:    0.8475"
+pos = text.find(':')
+p = text[pos+1:]
+end = float(p)
+print(end)
