f9d589848c300a46a7d9341158a98c5e0f7a4aa2
Priyankaj2503 <97089850+Priyankaj2503@users.noreply.github.com>
2022-06-30 10:34:06 +0530

    Update problem#13.py

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,20 @@
-# Network Programming
-# https://www.py4e.com/lessons/network
+name = input("Enter file:")
+handle = open(name)
+lst = list()
+for line in handle:
+    if not line.startswith("From:"):continue
+    line = line.split()
+    lst.append(line[1])
+counts = dict()
+for word in lst:
+	counts[word] = counts.get(word,0) + 1
+bigcount = None
+bigword = None
+for word,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigcount = count
+        bigword = word
+print(bigword , bigcount)
+
+
+
