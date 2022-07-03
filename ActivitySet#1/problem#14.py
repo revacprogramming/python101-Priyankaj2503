135739126a85f9e8cdda1054b11e72f51667dd86
Priyanka2503 <97089850+Priyankaj2503@users.noreply.github.com>
2022-07-03 14:54:08 +0000

    done

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,10 @@
-# Using Web Services
-# https://www.py4e.com/lessons/servces
+import re
+fh = input("Enter the file name:")
+fhand = open(fh)
+sum = 0
+for line in fhand:
+  num = re.findall("[0-9]+",line)
+  length = len(num)
+  for i in range(length):
+     sum = sum + int(num[i])
+print(sum)
